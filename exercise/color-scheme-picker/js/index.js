10533947885d87de2cb54d123a8fd123f78e2e7f
Defaf <defaf_kamfar@hotmail.com>
2019-05-01 22:22:22 +0300

    solve the website color picker

--- exercise/color-scheme-picker/js/index.js
+++ exercise/color-scheme-picker/js/index.js
@@ -1,9 +1,20 @@
 // make background gray
 // make font white
+$('#grayButton').click(function (){
+    $('body').css('background-color',"grey")  ;
+    $('h1').css("color","white");
+    $('p').css("color","white");
 
+})
 // When gray button is clicked 
 
 // make background white
 // make font black
 
 // When white button is clicked 
+$('#whiteButton').click(function (){
+    $('body').css('background-color',"white")  ;
+    $('h1').css("color","black");
+    $('p').css("color","black");
+
+})
