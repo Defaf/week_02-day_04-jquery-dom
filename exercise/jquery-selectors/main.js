b046d3cb643eddc28b3d462a62e9c367500c5cb9
Defaf <defaf_kamfar@hotmail.com>
2019-05-01 22:27:52 +0300

    add simple jQuery to list

--- exercise/jquery-selectors/main.js
+++ exercise/jquery-selectors/main.js
@@ -1,11 +1,15 @@
 // 1.  Select the H1
-let heading = ????
+let heading = $('h1')
 console.log(heading)
 
-// 2.  Select all of the list items
-let listItems = ????
+// // 2.  Select all of the list items
+let listItems = $('li"')
 console.log(listItems)
 
-// 3.  Select only the last list item
-let listItem = ????
+// // 3.  Select only the last list item
+let listItem = $("li:last-child") ;
+console.log(listItem)
+
+// another solution 
+listItem = $('li')[$("li").length]-1 ;
 console.log(listItem)
