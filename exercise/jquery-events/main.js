d4a2b91d4ca5149c5e936f9cd7d76e70d70e66a8
Defaf <defaf_kamfar@hotmail.com>
2019-05-01 22:23:17 +0300

    solve the event in jquery

--- exercise/jquery-events/main.js
+++ exercise/jquery-events/main.js
@@ -1,6 +1,8 @@
 // 1.  Add a click event to button that toggle show/hide of list
-
-
+function btnToggle(){
+    $('ul').toggle();
+}
+$('button').click(btnToggle);
 // 2.  Add a submit event to add-list-item form that adds user input to list
 
     // prevent the form from refreshing page
@@ -9,6 +11,13 @@
     // get value that user entered
 
     // add the user input as an li to the list
+
+$('button').click(function (event){
+    event.preventDefault();//stop refresh the page 
+    let city =  $("#new-list-item").val();
+    $('ul').append("<li>"+city+"</li>")  ;
+    $("#new-list-item:first-child").val("");
+});
 
 
 
@@ -21,8 +30,16 @@
 
     // update the image src with user input
 
-
+$('button').click(function (event){
+    event.preventDefault();//stop refresh the page 
+    let imgIcon =  $("#new-image").val();
+    $('img').attr("src",imgIcon )  ;
+});
+    
 
 // 4.  BONUS: Look up `event.target` and use it so that
 //     when a user clicks on a list item
 //     it changes just that list item to red
+$('li').on("click",function(event){
+    $(event.target).css("color","red");
+})
